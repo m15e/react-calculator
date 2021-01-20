e738cdc2a709a27b3e1fd40284093afd7a934c11
Mark Alexander <6917497+m15e@users.noreply.github.com>
2021-01-20 21:42:22 +0800

    Refactored code

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,11 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 const App = () => {
-  const [state, setState] = useState({ total: null, next: null, operation: null });
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
   const clickHandler = buttonName => {
     setState(state => calculate(state, buttonName));
@@ -20,34 +24,5 @@ const App = () => {
     </>
   );
 };
-
-// old class version
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       total: null,
-//       next: null,
-//       operation: null,
-//     };
-
-//     this.clickHandler = this.clickHandler.bind(this);
-//   }
-
-//   clickHandler(buttonName) {
-//     this.setState(state => calculate(state, buttonName));
-//   }
-
-//   render() {
-//     const { total, next, operation } = this.state;
-//     return (
-//       <>
-//         <Display result={{ total, next, operation }} />
-//         <ButtonPanel clickHandler={this.clickHandler} />
-//       </>
-//     );
-//   }
-// }
 
 export default App;
